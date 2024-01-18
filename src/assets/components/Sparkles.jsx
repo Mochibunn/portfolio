881693b6e82acaddf1c9dbafc511a56f55c240a3
Mochi Thornton <hi@mochibun.me>
2024-01-18 21:51:47 +0100

    Initial GSAP setup, practice

--- src/assets/components/Sparkles.jsx
+++ src/assets/components/Sparkles.jsx
@@ -1,10 +1,34 @@
+import { useRef } from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
 import "../styles/sparkles.css";
 import Sparkle from "../img/Sparkle.jsx";
 
 const Sparkles = () => {
+  const container = useRef();
+  useGSAP(
+    () => {
+      gsap.to(".star", {
+        y: "random(-50, 50)",
+        ease: "power2.inOut",
+        duration: "random(1.5, 2.2)",
+        repeat: -1,
+        yoyo: "true",
+        stagger: 0.23,
+      });
+    },
+    { scope: container }
+  );
+
   return (
-    <div className="glow relative z-20">
-      <div className="center z-10">
+    <div ref={container} className="relative z-20">
+      <div className="center z-10 star">
+        <Sparkle />
+      </div>
+      <div className="center z-10 star ml-20">
+        <Sparkle />
+      </div>
+      <div className="center z-10 star ml-40">
         <Sparkle />
       </div>
     </div>
--- src/assets/img/Sparkle.jsx
+++ src/assets/img/Sparkle.jsx
@@ -2,8 +2,8 @@ const Sparkle = () => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width="36"
-      height="36"
+      width="64"
+      height="64"
       viewBox="0 0 36 36"
     >
       <path
