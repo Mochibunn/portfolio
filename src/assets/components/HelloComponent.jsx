410f988ea099974f05e8e3860079776ce1b70308
Mochi Thornton <hi@mochibun.me>
2024-01-21 21:11:57 +0100

    GSAP WIP

--- src/assets/components/HelloComponent.jsx
+++ src/assets/components/HelloComponent.jsx
@@ -1,18 +1,37 @@
+import { useRef } from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { Parallax } from "react-scroll-parallax";
 import Sparkles from "./Sparkles";
 import SparkleComponent from "./SparkleComponent";
 
 const HelloComponent = () => {
+  const container = useRef();
+
+  useGSAP(
+    () => {
+      gsap.registerPlugin(ScrollTrigger);
+      gsap.from(".hello", {
+        scrollTrigger: {
+          trigger: ".hello",
+          scrub: 2,
+          start: "center center",
+          end: "top top",
+          markers: "true",
+          pin: true,
+        },
+        ease: "none",
+        opacity: 0,
+        duration: 1.5,
+      });
+    }, { scope: container }
+  );
+  
   return (
-    <div className="min-h-[400px] mx-4 pb-4">
+    <div className="min-h-[400px] mx-4 pb-4" ref={container}>
       <div className="flex justify-center relative">
         <Parallax speed={20}></Parallax>
-        <h1
-          className="text-[8rem] z-text py-6 font-mynerve font-bold"
-          id="hello"
-        >
-          Hello!
-        </h1>
         <div>
           <SparkleComponent
             minX={180}
@@ -32,6 +51,12 @@ const HelloComponent = () => {
             maxS={1.25}
             stagger={0.25}
           />
+        <h1
+          className="text-[8rem] z-text py-6 font-mynerve font-bold hello"
+          id="hello"
+        >
+          Hello!
+        </h1>
           <SparkleComponent
             minX={800}
             maxX={900}
@@ -41,11 +66,20 @@ const HelloComponent = () => {
             maxS={1.25}
             stagger={0.33}
           />
+          <SparkleComponent
+            minX={760}
+            maxX={860}
+            minY={220}
+            maxY={260}
+            minS={0.5}
+            maxS={1.25}
+            stagger={0.69}
+          />
         </div>
       </div>
       <p className="text-xl mb-2"></p>
-      <img src="../src/assets/img/1.jpg" className="rounded-xl" />
-      <p className="text-xl mt-2">
+      <img src="https://www.thesprucepets.com/thmb/PYJiButDwZb4JNzGE1gwfsI6YYs=/1733x0/filters:no_upscale():strip_icc()/Stocksy_txp14acff329Kw100_Medium_1360769-5aec7baefa6bcc00373c6cb7.jpg" className="rounded-xl w-[640px] mx-auto mb-12" />
+      <p className="text-xl mt-2 h-screen">
         This is just some more texty text to fill in this empty space in the
         div, you know! Lorem ipsum, dolor sit amet consectetur adipisicing elit.
         Quos reiciendis quia provident facilis commodi error, in rem. Rem earum
