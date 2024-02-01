1a852bdca76da9a0939cbd510ff74b64971f8b0d
Mochi Thornton <hi@mochibun.me>
2024-02-01 17:43:59 +0100

    FM animation WIP, good progress

--- src/assets/components/Parallax.jsx
+++ src/assets/components/Parallax.jsx
@@ -10,14 +10,21 @@ const Parallax = () => {
   const parallaxBg = useTransform(scrollYProgress, [0, 1], ["0%", "-10%"]);
   const parallaxTxt = useTransform(scrollYProgress, [0, 1], ["0%", "300%"]);
 
+  const scaling =
+    Math.round((window.devicePixelRatio + Number.EPSILON) * 100) / 100;
+
   return (
     <div
       className="relative grid w-full h-[220vh] overflow-hidden place-items-center"
       ref={ref}
     >
       <motion.h1
-        style={{ y: parallaxTxt }}
-        className="text-[8rem] font-singleday font-bold select-none pointer-events-none text-star relative z-10 bottom-10 md:bottom-0"
+        id="hello"
+        style={{
+          y: parallaxTxt,
+          top: -Math.round(80 * scaling),
+        }}
+        className="text-[30vw] xl:text-[20vw] 2xl:text-[10vw] font-singleday font-bold select-none pointer-events-none text-star relative z-10 bottom-0"
       >
         Hello!
       </motion.h1>
@@ -35,7 +42,8 @@ const Parallax = () => {
         style={{
           backgroundImage: "url(/bg-front.png)",
           backgroundPosition: "bottom",
-          // backgroundSize: "cover",
+          // backgroundSize: "150%",
+          backgroundRepeat: "no-repeat",
         }}
       />
     </div>
--- src/assets/components/Zoom.jsx
+++ src/assets/components/Zoom.jsx
@@ -1,11 +1,7 @@
-import { useRef } from "react";
-
 const Zoom = () => {
-  let zoom = useRef(((window.outerWidth - 10) / window.innerWidth) * 100);
-
   return (
-    <h1 className="px-4 py-2 mx-12 text-xl font-bold max-w-fit bg-black/30 rounded-xl">
-      Current page width: {window.innerWidth}px
+    <h1 className="px-4 py-2 mx-12 mt-4 text-xl font-bold max-w-fit bg-black/30 rounded-xl">
+      Monitor scaling: {Math.round((window.devicePixelRatio + Number.EPSILON) * 100) / 100}x
     </h1>
   );
 };
