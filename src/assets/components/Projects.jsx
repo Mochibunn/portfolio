28ec8076cceb0d6efeb8e7983524e91d182c189e
Mochi Thornton <hi@mochibun.me>
2024-02-09 21:22:13 +0100

    WIP

--- src/assets/components/Projects.jsx
+++ src/assets/components/Projects.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, useRef, } from "react";
+import { useEffect, useState, useRef } from "react";
 import {
   AnimatePresence,
   motion,
@@ -24,7 +24,7 @@ const Projects = () => {
   });
   const [scrollPos, setScrollPos] = useState(0);
   // console.log(scrollPos);
-  
+
   useEffect(() => {
     const mouseMove = (e) => {
       setMousePos({
@@ -35,7 +35,7 @@ const Projects = () => {
     window.addEventListener("mousemove", mouseMove);
     return () => {
       window.removeEventListener("mousemove", mouseMove);
-    }
+    };
   }, []);
 
   useEffect(() => {
@@ -47,12 +47,12 @@ const Projects = () => {
       } else if (scrollCurr < scrollPos) {
         //scroll up
       } else return;
-      setScrollPos(scrollCurr <=0 ? 0 : scrollCurr);
+      setScrollPos(scrollCurr <= 0 ? 0 : scrollCurr);
     };
     window.addEventListener("scroll", scrollCheck, false);
     return () => {
       window.removeEventListener("scroll", scrollCheck);
-    }
+    };
   }, [scrollPos]);
 
   return (
@@ -65,40 +65,40 @@ const Projects = () => {
         <h1 className="mb-12 text-9xl font-alegreya-bold">MY PROJECTS</h1>
         {/* Card component */}
         <div className="flex w-full px-12 justify-evenly rounded-2xl">
-          <div className="w-5/12 rounded-2xl">
+          <div className="sticky w-5/12 rounded-2xl top-20">
             <Tilt
-            tiltAngleXInitial={0}
-            tiltAngleYInitial={-10}
-            transitionSpeed={1000}
-            glareEnable={true}
-            glareBorderRadius="12px"
-            glareMaxOpacity={0.30}
-            glarePosition="right"
-            className="w-fit overflow-clip rounded-2xl"
+              tiltAngleXInitial={0}
+              tiltAngleYInitial={-10}
+              transitionSpeed={1000}
+              glareEnable={true}
+              glareBorderRadius="12px"
+              glareMaxOpacity={0.3}
+              glarePosition="right"
+              className="w-fit overflow-clip rounded-2xl"
             >
-            <div>
-              <img
-                src="./vinylC.webp"
-                alt="Website screenshot"
-                className="rounded-2xl"
-              />
-            </div>
-              </Tilt>
+              <div>
+                <img
+                  src="./vinylC.webp"
+                  alt="Website screenshot"
+                  className="rounded-2xl"
+                />
+              </div>
+            </Tilt>
           </div>
           <div className="block w-6/12">
-            <h1 className="text-6xl text-center font-alegreya-bold">Vinyl Countdown</h1>
-            <BubbleText
-              content="Vinyl countdown is my personal favorite. Most of the front end and some of the back end done by me. All of the albums available are from my physical vinyl collection."
-            />
-            <div className="flex mt-4">
-              <div>
+            <h1 className="text-6xl text-center font-alegreya-bold">
+              Vinyl Countdown
+            </h1>
+            <BubbleText content="Vinyl countdown is my personal favorite. Most of the front end and some of the back end done by me. All of the albums available are from my physical vinyl collection." />
+            <div className="relative flex mt-4 justify-stretch">
+              <div id="stack-gh-vc" className="min-w-5/12">
                 <h1 className="text-2xl font-alegreya-bold">TECH STACK:</h1>
-                <div className="flex gap-2">
+                <div className="flex gap-2" id="stack-container-vc">
                   <img
                     className="aspect-square w-[64px]"
                     src="./React.svg"
                     alt="React logo"
-                    />
+                  />
                   <img
                     className="aspect-square w-[64px]"
                     src="./Tailwind.svg"
@@ -108,15 +108,42 @@ const Projects = () => {
                     className="aspect-square w-[64px]"
                     src="./NextUI.svg"
                     alt="NextUI logo"
-                    />
-                  <div className="relative">
-                    <img 
+                  />
+                  <div className="relative" id="PostgreSQL-circle">
+                    <img
                       src="./circle.svg"
-                      className="aspect-square w-[64px]"
-
+                      className="aspect-square w-[64px] z-30 relative origin-center scale-[1.25]"
+                      alt="Scribble circle"
+                    />
+                    <img
+                      src="./PostgreSQL.svg"
+                      className="aspect-square w-[64px] z-20 absolute inset-0"
+                      alt="PostgreSQL logo"
                     />
                   </div>
-                  </div>
+                </div>
+                <h1 className="mt-4 text-2xl font-alegreya-bold">REPO:</h1>
+                <div className="relative">
+                  <img
+                    src="./github.svg"
+                    alt="Octocat logo"
+                    className="aspect-square w-[96px] mt-4 absolute -left-5"
+                  />
+                </div>
+              </div>
+              <div className="relative">
+                <img
+                  src="./arrow-orang.svg"
+                  className="h-[100px] relative -top-6"
+                />
+              </div>
+              <div className="min-w-5/12">
+                <h1 className="text-2xl font-alegreya-bold">ALTERNATE VERSION:</h1>
+                <img
+                  src="./vinylC.svg"
+                  alt="Viny Countdown logo"
+                  className="h-[64px]"
+                />
               </div>
             </div>
           </div>
