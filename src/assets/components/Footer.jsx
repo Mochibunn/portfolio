903ab911245bcb71c1fb274d1261fb08a7e6811d
Mochi Thornton <hi@mochibun.me>
2024-02-14 20:59:50 +0100

    Added notice, migrating to domain

--- src/assets/components/Footer.jsx
+++ src/assets/components/Footer.jsx
@@ -1,8 +1,9 @@
 const Footer = () => {
   return (
     <div className="bg-purp">
-      {/* <div id="divider" /> */}
-      <div className="h-[100px]">
+      <div id="divider" />
+      <div className="h-[100px] flex justify-center mt-6">
+        <p className="italic text-white/30">Don&apos;t worry, my classic silly webpage will be available at a new special location, it is not lost to time!🐰</p>
       </div>
     </div>
   );
